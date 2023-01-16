e141988ce1e149cbe8b57f25179c91181a0940db
batmanscode <29989939+batmanscode@users.noreply.github.com>
2023-01-16 09:13:54 +0000

    dependencies now live in `setup.py`
    pip installing `requirements.txt` still works and will install dependencies from `setup.py` in editable mode

--- setup.py
+++ setup.py
@@ -41,7 +41,12 @@ setuptools.setup(
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
     include_package_data = True,
-    install_requires = requirements,
+    # install_requires = requirements,
+    install_requires = [
+        'pandas==1.2.5',
+        'deta==1.1.0',
+        'ulid-py==1.1.0',
+    ],
     extras_require={ 'dev': dev_requirements },
     dependency_links = cfg.get('dep_links','').split(),
     python_requires  = '>=' + cfg['min_python'],
